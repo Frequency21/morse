44a905ba93691bece8ac56e4fc84043bdd5e015f
Frequency21 <K.Oliver.XCVII@gmail.com>
2025-01-09 18:46:12 +0100

    formatting + roboto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styles: [],
 })
 export class AppComponent {
-  title = 'morse';
+    title = 'morse'
 }
