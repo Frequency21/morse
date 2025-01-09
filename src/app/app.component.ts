c2e60c1a34e0f08b54a6afcfa5b23e22552dc15c
Frequency21 <K.Oliver.XCVII@gmail.com>
2025-01-09 18:46:28 +0100

    WIP
    adds keyboards
    and morse code table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, CommonModule],
     templateUrl: './app.component.html',
-    styles: [],
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
     title = 'morse'
+
+    keyboardRows = [
+        "qwertyuiop".split(''),
+        "asdfghjkl".split(''),
+        "zxcvbnm".split(''),
+    ]
+
+    onMouseDown(event: MouseEvent) {
+        (event.target as HTMLElement).classList.add('pressed')
+    }
+
+    onMouseUp(event: MouseEvent) {
+        (event.target as HTMLElement).classList.remove('pressed')
+    }
 }
