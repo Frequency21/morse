7a54ed803c02d837d558ae0e808af0dae82f52ba
Frequency21 <K.Oliver.XCVII@gmail.com>
2025-01-14 15:40:38 +0100

    adds keyboard and morse code table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,59 @@
-import { CommonModule } from '@angular/common'
-import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { filter, map, of } from 'rxjs';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, CommonModule],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  host: {
+    '(document:keyup)': 'onKeyboardUp($event)',
+    '(document:keydown)': 'onKeyboardDown($event)',
+  },
 })
 export class AppComponent {
-    title = 'morse'
+  title = 'morse';
 
-    keyboardRows = [
-        "qwertyuiop".split(''),
-        "asdfghjkl".split(''),
-        "zxcvbnm".split(''),
-    ]
+  keyboardRows = [
+    'qwertyuiop'.split(''),
+    'asdfghjkl'.split(''),
+    'zxcvbnm'.split(''),
+    ['space'],
+  ];
 
-    onMouseDown(event: MouseEvent) {
-        (event.target as HTMLElement).classList.add('pressed')
-    }
+  onKeyboardUp(event: KeyboardEvent) {
+    if (event.key.length !== 1) return;
+    let key = event.key.toLowerCase();
 
-    onMouseUp(event: MouseEvent) {
-        (event.target as HTMLElement).classList.remove('pressed')
-    }
+    const charCode = key.charCodeAt(0);
+
+    if (charCode === 32) {
+      key = 'space';
+    } else if (charCode < 97 || charCode > 122) return;
+
+    document.querySelector(`[data-letter=${key}]`)?.classList.remove('pressed');
+  }
+
+  onKeyboardDown(event: KeyboardEvent) {
+    if (event.key.length !== 1) return;
+    let key = event.key.toLowerCase();
+
+    const charCode = key.charCodeAt(0);
+
+    if (charCode === 32) {
+      key = 'space';
+    } else if (charCode < 97 || charCode > 122) return;
+
+    document.querySelector(`[data-letter=${key}]`)?.classList.add('pressed');
+  }
+
+  onMouseDown(event: MouseEvent) {
+    (event.target as HTMLElement).classList.add('pressed');
+  }
+
+  onMouseUp(event: MouseEvent) {
+    (event.target as HTMLElement).classList.remove('pressed');
+  }
 }
